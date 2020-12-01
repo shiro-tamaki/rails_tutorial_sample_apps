769a8e6eded1ae21580627212e6eda04640a333a
shiro-tamaki <ubuntu@ip-172-31-61-175.ec2.internal>
2020-12-01 12:57:12 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users
 end
-
-
-# コメント、gitちゃんと保存できたかのチェック用
-# 米米コメント。米米
