ef02bfa7150cbc5c370c036062a1837a601c72b6
shiro-tamaki <ubuntu@ip-172-31-61-175.ec2.internal>
2020-12-03 01:25:03 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,7 @@
-require 'test_helper'
+  require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -10,11 +11,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#error_explanation'
-    assert_select 'div.alert' 
   end
 
- test "valid signup information" do
+  test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
@@ -24,6 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
